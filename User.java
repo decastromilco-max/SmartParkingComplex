19732c0f530d9da20271152b20cf129912572a57
erlsalvador106-cmd <erlsalvador106@gmail.com>
2026-05-05 15:44:44 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,18 @@
+public abstract class User {
+    private String name;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String name, String username, String password, String role) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName() { return name; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+}
